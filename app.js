afb0863cf731e1b0f065d1e0e38072801e5eeb10
omdwd74 <omjidwivedi123@gmail.com>
2023-09-02 15:22:22 +0530

    YELP

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const ejsMate= require('ejs-mate');
+const session = require('express-session');
+
+const ExpressError = require('./utils/ExpressError');
+
+const flash = require('connect-flash');
+
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
+// app.use(express.static(path.join(__dirname, 'public')))
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    useUnifiedTopology:true
+    // useFindAndModify : false
+    // useFindAndModify: false,
+});
+
+const db= mongoose.connection;
+db.on("error",console.error.bind(console,"connnection error"));
+db.once("open",()=>{
+    console.log("Database connected");
+
+})
+app.engine('ejs',ejsMate)
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'));
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+const sessionConfig = {
+    secret: 'thisshouldbeabettersecret!',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.success = req.flash('success');
+    res.locals.error = req.flash('error');
+    next();
+})
+
+
+app.use('/campgrounds',campgrounds);
+app.use('/campgrounds/:id/reviews',reviews);
+
+app.get('/',(req,res)=>{
+    res.render('home');tcgvh
+
+})
+
+app.all('*',(req,res,next)=>{
+    // res.send('404 not found ')
+    next(new ExpressError('Page Not found',404))
+})
+app.use((err,req,res,next)=>{
+    const {statusCode=500,message = 'Something went wrong!!'}= err;
+   if(!err.message) err.message = 'Oh no, Something went wrong !!'
+    res.status(statusCode).render('error',{err});
+    // res.send('Oh budy something is wrong');
+})
+app.listen(3000,()=>{
+    console.log("Listen on port 3000")
+})
+
+
+
